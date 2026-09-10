44c3c6bb710da8268c03236220a4ecafb366cdf0
Hudzaifah <ujai757@gmail.com>
2026-09-10 10:46:52 +0700

    chore: update database seeding command to execute the production build file

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 echo "Running database migrations..."
 cd /app/server
 npm run db:push
-npm run db:seed
+node dist/db/seed.js
 
 echo "Starting Express API on port 4000..."
 PORT=4000 node dist/index.js &
